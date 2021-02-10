797e1ab7bb7449811303f50739545d9015704b23
miyanokomiya <tomoya.hstexze24632@gmail.com>
2021-02-10 14:09:40 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "@vue/typescript",
+    "plugin:prettier/recommended",
+    "prettier/vue",
+    "prettier/@typescript-eslint",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {},
+};

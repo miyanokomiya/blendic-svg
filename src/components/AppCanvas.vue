f2d1680679cab6f0d7098cc3cb2426934fe98433
miyanokomiya <tomoya.hstexze24632@gmail.com>
2021-02-12 23:51:49 +0900

    grid axis

--- src/components/AppCanvas.vue
+++ src/components/AppCanvas.vue
@@ -20,6 +20,7 @@
       @keydown.g.exact.prevent="editKeyDown('g')"
       @keydown.e.exact.prevent="editKeyDown('e')"
       @keydown.x.exact.prevent="editKeyDown('x')"
+      @keydown.y.exact.prevent="editKeyDown('y')"
       @keydown.a.shift.exact.prevent="editKeyDown('shift-a')"
     >
       <rect
@@ -32,16 +33,26 @@
         stroke-width="1"
         stroke-dasharray="4 4"
       ></rect>
+      <line
+        v-if="gridLineElm"
+        :x1="gridLineElm.from.x"
+        :y1="gridLineElm.from.y"
+        :x2="gridLineElm.to.x"
+        :y2="gridLineElm.to.y"
+        :stroke="gridLineElm.stroke"
+        :stroke-width="gridLineElm.strokeWidth"
+      ></line>
       <slot />
     </svg>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref, reactive, computed } from 'vue'
+import { defineComponent, PropType, ref, reactive, computed, watch } from 'vue'
 import { getPointInTarget } from 'okanvas'
 import { IVec2, IRectangle, multi, sub, add, getRectCenter } from 'okageo'
 import { CanvasCommand } from '/@/models'
+import * as helpers from '/@/utils/helpers'
 
 export default defineComponent({
   props: {
@@ -62,6 +73,7 @@ export default defineComponent({
     'g',
     'e',
     'x',
+    'y',
     'shift-a',
   ],
   setup(props, { emit }) {
@@ -71,25 +83,55 @@ export default defineComponent({
     const mousePoint = ref<IVec2>()
     const scale = ref(1)
     const viewOrigin = ref<IVec2>({ x: 0, y: 0 })
-    const viewBox = computed(
-      () =>
-        `${viewOrigin.value.x} ${viewOrigin.value.y} ${
-          viewSize.width * scale.value
-        } ${viewSize.height * scale.value}`
-    )
-    const viewCenter = computed(() =>
-      getRectCenter({ x: 0, y: 0, ...viewSize })
-    )
     const viewMovingInfo = ref<{ origin: IVec2; downAt: IVec2 }>()
+    const axisGrid = ref<'' | 'x' | 'y'>('')
 
     function viewToCanvas(v: IVec2): IVec2 {
       return multi(v, scale.value)
     }
 
+    const viewCanvasRect = computed(() => ({
+      x: viewOrigin.value.x,
+      y: viewOrigin.value.y,
+      width: viewSize.width * scale.value,
+      height: viewSize.height * scale.value,
+    }))
+
+    const viewBox = computed(
+      () =>
+        `${viewCanvasRect.value.x} ${viewCanvasRect.value.y} ${viewCanvasRect.value.width} ${viewCanvasRect.value.height}`
+    )
+
+    const viewCenter = computed(() =>
+      getRectCenter({ x: 0, y: 0, ...viewSize })
+    )
+
+    const gridLineElm = computed(() => {
+      if (axisGrid.value === '') return
+      if (!editStartPoint.value) return
+      return helpers.gridLineElm(
+        scale.value,
+        axisGrid.value,
+        viewCanvasRect.value,
+        editStartPoint.value
+      )
+    })
+
+    watch(
+      () => props.currentCommand,
+      (to) => {
+        if (to === '') {
+          editStartPoint.value = undefined
+          axisGrid.value = ''
+        }
+      }
+    )
+
     return {
       viewSize,
       svg,
       viewBox,
+      gridLineElm,
       focus() {
         if (svg.value) svg.value.focus()
       },
@@ -115,6 +157,7 @@ export default defineComponent({
       },
       mousemove: (e: MouseEvent) => {
         mousePoint.value = getPointInTarget(e)
+
         if (viewMovingInfo.value) {
           viewOrigin.value = add(
             viewMovingInfo.value.origin,
@@ -122,6 +165,19 @@ export default defineComponent({
           )
         } else {
           if (!editStartPoint.value) return
+
+          if (axisGrid.value === 'x') {
+            mousePoint.value = {
+              x: mousePoint.value.x,
+              y: editStartPoint.value.y,
+            }
+          } else if (axisGrid.value === 'y') {
+            mousePoint.value = {
+              x: editStartPoint.value.x,
+              y: mousePoint.value.y,
+            }
+          }
+
           emit('mousemove', {
             current: viewToCanvas(mousePoint.value),
             start: viewToCanvas(editStartPoint.value),
@@ -139,11 +195,17 @@ export default defineComponent({
       keyDownTab: () => {
         emit('tab')
       },
-      editKeyDown(key: 'g' | 'e' | 'x' | 'shift-a') {
+      editKeyDown(key: 'g' | 'e' | 'x' | 'y' | 'shift-a') {
         if (!mousePoint.value) return
         if (([''] as CanvasCommand[]).includes(props.currentCommand)) {
           editStartPoint.value = mousePoint.value
           emit(key)
+        } else if (
+          (['grab'] as CanvasCommand[]).includes(props.currentCommand)
+        ) {
+          if (key === 'x') axisGrid.value = axisGrid.value === 'x' ? '' : 'x'
+          else if (key === 'y')
+            axisGrid.value = axisGrid.value === 'y' ? '' : 'y'
         }
       },
     }
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,5 @@
-import { IVec2, Transform } from '../models/index'
+import { IVec2, IRectangle } from 'okageo'
+import { Transform } from '../models/index'
 
 export function transform(transform: Transform): string {
   return [
@@ -17,4 +18,33 @@ export function d(points: IVec2[], closed = false): string {
     body.map((p) => `L${p.x},${p.y}`).join('') +
     (closed ? 'z' : '')
   )
+}
+
+export function gridLineElm(
+  scale: number,
+  axis: 'x' | 'y',
+  viewCanvasRect: IRectangle,
+  editStartPoint: IVec2
+) {
+  const base = { strokeWidth: scale }
+  if (axis === 'x')
+    return {
+      ...base,
+      from: { x: viewCanvasRect.x, y: editStartPoint.y },
+      to: {
+        x: viewCanvasRect.x + viewCanvasRect.width,
+        y: editStartPoint.y,
+      },
+      stroke: 'red',
+    }
+  else
+    return {
+      ...base,
+      from: { x: editStartPoint.x, y: viewCanvasRect.y },
+      to: {
+        x: editStartPoint.x,
+        y: viewCanvasRect.y + viewCanvasRect.height,
+      },
+      stroke: 'green',
+    }
 }

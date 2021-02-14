5b324698dc8829ca9b432ce5b5f6bf446533cde4
miyanokomiya <tomoya.hstexze24632@gmail.com>
2021-02-14 09:47:24 +0900

    import ress

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  alias: [{ find: '/@', replacement: path.resolve(__dirname, 'src') }],
+  resolve: {
+    alias: [{ find: '/@', replacement: path.resolve(__dirname, 'src') }],
+  },
 })

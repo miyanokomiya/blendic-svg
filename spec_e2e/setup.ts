f37290a082cd150255229eb919279cabb6a98f76
miyanokomiya <tomoya.hstexze24632@gmail.com>
2021-04-24 16:50:50 +0900

    chore: upload e2e snapshots

--- spec_e2e/setup.ts
+++ spec_e2e/setup.ts
@@ -1,6 +1,8 @@
 import { spawn, execSync } from 'child_process'
 
 module.exports = () => {
-  execSync('yarn build')
+  if (!process.env.E2E_SKIP_BUILD) {
+    execSync('yarn build')
+  }
   ;(global as any).__server__ = spawn('yarn', ['serve', '--port', '3333'])
 }

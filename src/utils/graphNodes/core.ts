6b26d70f38319dca394d954ec57f4a46f709d97b
miyanokomiya <tomoya.hstexze24632@gmail.com>
2021-06-23 22:52:27 +0900

    chore: add license

--- src/utils/graphNodes/core.ts
+++ src/utils/graphNodes/core.ts
@@ -1,3 +1,22 @@
+/*
+This file is part of Blendic SVG.
+
+Blendic SVG is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Blendic SVG is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Blendic SVG.  If not, see <https://www.gnu.org/licenses/>.
+
+Copyright (C) 2021, Tomoya Komiyama.
+*/
+
 import { Transform } from '/@/models'
 import { GraphNodeBase, GRAPH_VALUE_TYPE } from '/@/models/graphNode'
 

eb291564bc66a1e2275ef8f71f1eea0ec4757c1d
miyanokomiya <tomoya.hstexze24632@gmail.com>
2021-09-29 00:04:20 +0900

    feat: create normalized entities models

---
+++ spec/models/entity.spec.ts
@@ -0,0 +1,91 @@
+import * as target from '../../src/models/entity'
+
+describe('src/models/entity.ts', () => {
+  describe('addEntity', () => {
+    it('should add new entity', () => {
+      expect(
+        target.addEntity(
+          {
+            byId: { a: { id: 'a' } },
+            allIds: ['a'],
+          },
+          { id: 'b' }
+        )
+      ).toEqual({
+        byId: { a: { id: 'a' }, b: { id: 'b' } },
+        allIds: ['a', 'b'],
+      })
+    })
+  })
+
+  describe('updateEntity', () => {
+    it('should update the entity', () => {
+      expect(
+        target.updateEntity(
+          {
+            byId: { a: { id: 'a' }, b: { id: 'b', val: 1 } },
+            allIds: ['a', 'b'],
+          },
+          { id: 'b', val: 2 }
+        )
+      ).toEqual({
+        byId: { a: { id: 'a' }, b: { id: 'b', val: 2 } },
+        allIds: ['a', 'b'],
+      })
+    })
+  })
+
+  describe('updateEntities', () => {
+    it('should update some entities', () => {
+      expect(
+        target.updateEntities(
+          {
+            byId: { a: { id: 'a', val: 0 }, b: { id: 'b', val: 1 } },
+            allIds: ['a', 'b'],
+          },
+          {
+            a: { id: 'a', val: 1 },
+            b: { id: 'b', val: 2 },
+          }
+        )
+      ).toEqual({
+        byId: { a: { id: 'a', val: 1 }, b: { id: 'b', val: 2 } },
+        allIds: ['a', 'b'],
+      })
+    })
+  })
+
+  describe('removeEntity', () => {
+    it('should remove the entity', () => {
+      expect(
+        target.removeEntity(
+          {
+            byId: { a: { id: 'a' }, b: { id: 'b' } },
+            allIds: ['a', 'b'],
+          },
+          'b'
+        )
+      ).toEqual({
+        byId: { a: { id: 'a' } },
+        allIds: ['a'],
+      })
+    })
+  })
+
+  describe('removeEntities', () => {
+    it('should remove some entities', () => {
+      expect(
+        target.removeEntities(
+          {
+            byId: { a: { id: 'a' }, b: { id: 'b' }, c: { id: 'c' } },
+            allIds: ['a', 'b', 'c'],
+          },
+          ['b', 'c']
+        )
+      ).toEqual({
+        byId: { a: { id: 'a' } },
+        allIds: ['a'],
+      })
+    })
+  })
+})
---
+++ src/models/entity.ts
@@ -0,0 +1,69 @@
+import { IdMap } from '/@/models'
+import { mapFilter, reduceToMap } from '/@/utils/commons'
+
+export interface Entity {
+  id: string
+}
+
+export interface Entities<T extends Entity> {
+  byId: IdMap<T>
+  allIds: string[]
+}
+
+export function addEntity<T extends Entity>(
+  src: Entities<T>,
+  entity: T
+): Entities<T> {
+  return {
+    byId: { ...src.byId, [entity.id]: entity },
+    allIds: [...src.allIds, entity.id],
+  }
+}
+
+export function updateEntity<T extends Entity>(
+  src: Entities<T>,
+  entity: T
+): Entities<T> {
+  return {
+    byId: {
+      ...src.byId,
+      [entity.id]: entity,
+    },
+    allIds: src.allIds,
+  }
+}
+
+export function updateEntities<T extends Entity>(
+  src: Entities<T>,
+  entitiesById: IdMap<T>
+): Entities<T> {
+  return {
+    byId: { ...src.byId, ...entitiesById },
+    allIds: src.allIds,
+  }
+}
+
+export function removeEntity<T extends Entity>(
+  src: Entities<T>,
+  id: string
+): Entities<T> {
+  return {
+    byId: mapFilter(src.byId, (_, key) => key !== id),
+    allIds: src.allIds.filter((key) => key !== id),
+  }
+}
+
+export function removeEntities<T extends Entity>(
+  src: Entities<T>,
+  ids: string[]
+): Entities<T> {
+  const targets = reduceToMap(ids, () => true)
+  const allIds = src.allIds.filter((key) => !targets[key])
+  return {
+    byId: allIds.reduce<Entities<T>['byId']>((p, key) => {
+      p[key] = src.byId[key]
+      return p
+    }, {}),
+    allIds,
+  }
+}

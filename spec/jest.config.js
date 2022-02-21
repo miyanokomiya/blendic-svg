13b1d843d809ff9a6d76e0cc59097d3b8b3203c5
miyanokomiya <tomoya.hstexze24632@gmail.com>
2022-02-21 12:25:15 +0900

    chore: Switch from `ts-jest` to `@swc/jest`
    - looks twice faster

--- spec/jest.config.js
+++ spec/jest.config.js
@@ -1,25 +1,24 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils')
 const { compilerOptions } = require('../tsconfig')
 
 module.exports = {
   testEnvironment: 'jsdom',
   roots: ['<rootDir>/../'],
   modulePaths: ['<rootDir>/../'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/../',
-  }),
-
+  moduleNameMapper: Object.entries(compilerOptions.paths).reduce(
+    (p, [key, value]) => {
+      p[key.replace('*', '(.*)$')] = value.map(
+        (v) => `<rootDir>/../${v.replace('*', '$1')}`
+      )
+      return p
+    },
+    {}
+  ),
   moduleFileExtensions: ['ts', 'js', 'vue'],
-  preset: 'ts-jest',
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': '@swc/jest',
     '^.+\\.vue$': 'vue3-jest',
   },
   globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-      babelConfig: true,
-    },
     NODE_ENV: 'test',
   },
   testMatch: ['<rootDir>/**/*.spec.ts'],

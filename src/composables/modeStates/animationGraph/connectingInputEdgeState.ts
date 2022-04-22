b0e09d03892c93f5c4cb3d728e46e0349406ea0d
miyanokomiya <tomoya.hstexze24632@gmail.com>
2022-04-22 19:29:33 +0900

    refactor: Disconnect an edge

--- src/composables/modeStates/animationGraph/connectingInputEdgeState.ts
+++ src/composables/modeStates/animationGraph/connectingInputEdgeState.ts
@@ -26,6 +26,7 @@ import {
 } from '/@/composables/modeStates/animationGraph/utils'
 import { useAddingNewNodeState } from '/@/composables/modeStates/animationGraph/addingNewNodeState'
 import { usePanningState } from '/@/composables/modeStates/animationGraph/panningState'
+import { getUpdatedNodeMapToDisconnectNodeInput } from '/@/utils/graphNodes'
 
 export function useConnectingInputEdgeState(options: {
   nodeId: string
@@ -71,14 +72,27 @@ export function useConnectingInputEdgeState(options: {
             if (event.target.type === 'empty') {
               const draftEdge = ctx.getDraftEdge()
               if (draftEdge?.type === 'draft-from') {
-                return () =>
-                  useAddingNewNodeState({
-                    point: draftEdge.from,
-                    connect: {
-                      nodeId: draftEdge.to.nodeId,
-                      inputKey: draftEdge.to.key,
-                    },
-                  })
+                const nodeMap = ctx.getNodeMap()
+                const node = nodeMap[draftEdge.to.nodeId]
+                if (node.inputs[draftEdge.to.key].from) {
+                  ctx.updateNodes(
+                    getUpdatedNodeMapToDisconnectNodeInput(
+                      ctx.getGraphNodeModule,
+                      nodeMap,
+                      node.id,
+                      draftEdge.to.key
+                    )
+                  )
+                } else {
+                  return () =>
+                    useAddingNewNodeState({
+                      point: draftEdge.from,
+                      connect: {
+                        nodeId: draftEdge.to.nodeId,
+                        inputKey: draftEdge.to.key,
+                      },
+                    })
+                }
               }
             } else if (event.target.type === 'node-edge-output') {
               const nodeMap = ctx.getNodeMap()

5dba8a292ad0580329c408209c4cbc2e925e3be2
miyanokomiya <tomoya.hstexze24632@gmail.com>
2022-07-14 22:34:00 +0900

    fix: Make random ID format more strict for compatibility
    - `_` shouldn't be used for SVG

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,5 +1,12 @@
-import { nanoid } from 'nanoid'
+import { customAlphabet } from 'nanoid'
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+const nanoid = customAlphabet(
+  alphabet + alphabet.toUpperCase() + '0123456789-',
+  12
+)
 
 // Avoid `crypto.randomUUID` due to HTTPS limitation
 // Avoid number prefix for DOM ID
-export const generateUuid: () => string = () => `b${nanoid(12)}`
+// This ID should be compatible with CSS selector and SVG ID
+export const generateUuid: () => string = () => `b${nanoid()}`
